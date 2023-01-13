8a942dd0bca9b5601ec5742be2ff23dd593cd351
samuelho79 <106785600+sameow7979@users.noreply.github.com>
2023-01-13 22:59:29 +0800

    Petclinic application with Otel Instrumentation to Elasticsearch
    Petclinic application with Otel Instrumentation to Elasticsearch

---
+++ scripts/run_all.sh
@@ -0,0 +1,78 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o errtrace
+set -o nounset
+set -o pipefail
+
+pkill -9 -f spring-petclinic || echo "Failed to kill any apps"
+
+docker-compose kill || echo "No docker containers are running"
+
+echo "Running infra"
+docker-compose up -d grafana-server prometheus-server tracing-server
+
+echo "Running apps"
+mkdir -p target
+nohup java -javaagent:opentelemetry-javaagent.jar \
+-Dotel.resource.attributes="service.name=config_server,service.version=1.1,deployment.environment=production" \
+-Dotel.exporter.otlp.endpoint="https://abc40654749f4311bf845a8f8e4461d0.apm.ap-southeast-1.aws.cloud.es.io:443" \
+-Dotel.exporter.otlp.headers="Authorization=Bearer YldnKGU3uvzjQkY4Of" \
+-Dotel.metrics.exporter="otlp" \
+-Dotel.logs.exporter="otlp" \
+-Dotel.traces.exporter="otlp" \
+-jar spring-petclinic-config-server/target/*.jar --server.port=8888 --spring.profiles.active=chaos-monkey > target/config-server.log 2>&1 &
+echo "Waiting for config server to start"
+sleep 20
+nohup java -javaagent:opentelemetry-javaagent.jar \
+-Dotel.resource.attributes="service.name=discovery_server,service.version=1.1,deployment.environment=production" \
+-Dotel.exporter.otlp.endpoint="https://abc40654749f4311bf845a8f8e4461d0.apm.ap-southeast-1.aws.cloud.es.io:443" \
+-Dotel.exporter.otlp.headers="Authorization=Bearer YldnKGU3uvzjQkY4Of" \
+-Dotel.metrics.exporter="otlp" \
+-Dotel.logs.exporter="otlp" \
+-Dotel.traces.exporter="otlp" \
+-jar spring-petclinic-discovery-server/target/*.jar --server.port=8761 --spring.profiles.active=chaos-monkey > target/discovery-server.log 2>&1 &
+echo "Waiting for discovery server to start"
+sleep 20
+nohup java -javaagent:opentelemetry-javaagent.jar \
+-Dotel.resource.attributes="service.name=customers_server,service.version=1.1,deployment.environment=production" \
+-Dotel.exporter.otlp.endpoint="https://abc40654749f4311bf845a8f8e4461d0.apm.ap-southeast-1.aws.cloud.es.io:443" \
+-Dotel.exporter.otlp.headers="Authorization=Bearer YldnKGU3uvzjQkY4Of" \
+-Dotel.metrics.exporter="otlp" \
+-Dotel.logs.exporter="otlp" \
+-Dotel.traces.exporter="otlp" \
+-jar spring-petclinic-customers-service/target/*.jar --server.port=8081 --spring.profiles.active=chaos-monkey > target/customers-service.log 2>&1 &
+nohup java -javaagent:opentelemetry-javaagent.jar \
+-Dotel.resource.attributes="service.name=visits_server,service.version=1.1,deployment.environment=production" \
+-Dotel.exporter.otlp.endpoint="https://abc40654749f4311bf845a8f8e4461d0.apm.ap-southeast-1.aws.cloud.es.io:443" \
+-Dotel.exporter.otlp.headers="Authorization=Bearer YldnKGU3uvzjQkY4Of" \
+-Dotel.metrics.exporter="otlp" \
+-Dotel.logs.exporter="otlp" \
+-Dotel.traces.exporter="otlp" \
+-jar spring-petclinic-visits-service/target/*.jar --server.port=8082 --spring.profiles.active=chaos-monkey > target/visits-service.log 2>&1 &
+nohup java -javaagent:opentelemetry-javaagent.jar \
+-Dotel.resource.attributes="service.name=vets_server,service.version=1.1,deployment.environment=production" \
+-Dotel.exporter.otlp.endpoint="https://abc40654749f4311bf845a8f8e4461d0.apm.ap-southeast-1.aws.cloud.es.io:443" \
+-Dotel.exporter.otlp.headers="Authorization=Bearer YldnKGU3uvzjQkY4Of" \
+-Dotel.metrics.exporter="otlp" \
+-Dotel.logs.exporter="otlp" \
+-Dotel.traces.exporter="otlp" \
+-jar spring-petclinic-vets-service/target/*.jar --server.port=8083 --spring.profiles.active=chaos-monkey > target/vets-service.log 2>&1 &
+nohup java -javaagent:opentelemetry-javaagent.jar \
+-Dotel.resource.attributes="service.name=gateway_server,service.version=1.1,deployment.environment=production" \
+-Dotel.exporter.otlp.endpoint="https://abc40654749f4311bf845a8f8e4461d0.apm.ap-southeast-1.aws.cloud.es.io:443" \
+-Dotel.exporter.otlp.headers="Authorization=Bearer YldnKGU3uvzjQkY4Of" \
+-Dotel.metrics.exporter="otlp" \
+-Dotel.logs.exporter="otlp" \
+-Dotel.traces.exporter="otlp" \
+-jar spring-petclinic-api-gateway/target/*.jar --server.port=8080 --spring.profiles.active=chaos-monkey > target/gateway-service.log 2>&1 &
+nohup java -javaagent:opentelemetry-javaagent.jar \
+-Dotel.resource.attributes="service.name=admin_server,service.version=1.1,deployment.environment=production" \
+-Dotel.exporter.otlp.endpoint="https://abc40654749f4311bf845a8f8e4461d0.apm.ap-southeast-1.aws.cloud.es.io:443" \
+-Dotel.exporter.otlp.headers="Authorization=Bearer YldnKGU3uvzjQkY4Of" \
+-Dotel.metrics.exporter="otlp" \
+-Dotel.logs.exporter="otlp" \
+-Dotel.traces.exporter="otlp" \
+-jar spring-petclinic-admin-server/target/*.jar --server.port=9090 --spring.profiles.active=chaos-monkey > target/admin-server.log 2>&1 &
+echo "Waiting for apps to start"
+sleep 60
